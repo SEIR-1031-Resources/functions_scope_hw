ab59169ffc359de0262e289d6ce87e9ec8d781db
mary3210 <maryrodri3210@gmail.com>
2022-11-04 16:17:09 -0500

    Commit 1 - Verbal questions

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 // 1.1. What is the difference between a parameter and an argument?
-
+// A parameter is the variables that are defined inside a function. 
+// An arguement is the value that gets tossed into the function().
 // 1.2. Within a function, what is the difference between return and console.log?
-
+//console.log prints to the console/terminal. Return Holds the value after the function 
+//has ended and breaks away from the function once the code hits return. 
+//Returning doesnt mean it will print to the console.
 // 1. 3. What are the implications of the ability of a function to return a value?
+//the function will ceast to run anymore and the value that
+// was return will live outside the function.
 
 // 2. calculateCube
 function calculateCube(num) {
     // YOUR CODE HERE
+    
 }
 
 // 3. isAVowel
