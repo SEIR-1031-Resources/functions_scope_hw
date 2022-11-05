a1bd7496f59df869d787f8a63041b153d2c655f5
davidhlee94 <davidhlee94@gmail.com>
2022-11-04 21:48:15 -0700

    Complete problem 2

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-// 1.1. What is the difference between a parameter and an argument?
-let parameter = 'parameters are listed in the functions definition and basically act as a method of declaration for the argument'
+// // 1.1. What is the difference between a parameter and an argument?
+// let parameter = 'parameters are listed in the functions definition and basically act as a method of declaration for the argument'
 
-let argument = 'arguments are the values that pass through the function when the function is invoked. This will follow the format of the parameter.'
+// let argument = 'arguments are the values that pass through the function when the function is invoked. This will follow the format of the parameter.'
 
-// 1.2. Within a function, what is the difference between return and console.log?
-let consoleLog = 'console.log will log the output, but will continue to execute the rest of the code until a return statement is ran'
+// // 1.2. Within a function, what is the difference between return and console.log?
+// let consoleLog = 'console.log will log the output, but will continue to execute the rest of the code until a return statement is ran'
 
-let return = 'return statements will log the output, stop the execution of the function code, and let other codes use the value passed by the return.'
+// let return = 'return statements will log the output, stop the execution of the function code, and let other codes use the value passed by the return.'
 
-// 1. 3. What are the implications of the ability of a function to return a value?
-'A function has the ability to return a value if you call the function outside of the function, and if your function provides an output.'
+// // 1. 3. What are the implications of the ability of a function to return a value?
+// 'A function has the ability to return a value if you call the function outside of the function, and if your function provides an output.'
 
-// // 2. calculateCube
-// function calculateCube(num) {
-//     // YOUR CODE HERE
-// }
+// 2. calculateCube
+function calculateCube(num) {
+       let cubed = num ** 3
+       return cubed
+}
+console.log(calculateCube(5));
+
 
 // // 3. isAVowel
 // function isAVowel(letter) {
