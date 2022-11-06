2bdb525536762d41adfd5113660351a2dd0ba1a6
ShadowTorn33 <austincheer94@gmail.com>
2022-11-06 09:55:11 -0800

    Longest Word

--- app.js
+++ app.js
@@ -65,8 +65,15 @@ console.log(printPrimes(97))
 
 // 7. printLongestWord
 function printLongestWord(arr) {
-    // YOUR CODE HERE
+    let longestWord = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        if (longestWord.length < arr[i].length) {
+            longestWord = arr[i]
+        }
+    } return longestWord
 }
+
+console.log(printLongestWord(["BoJack", "Princess", "Diane", "a", "Max", "Peanutbutter", "big", "blob"]))
 
 // BONUS!
 
