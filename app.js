addd5370d6629b44b4c1a9bc62e2e32ea440d164
solsarr <solomonsarr18@gmail.com>
2022-11-05 22:43:29 -0400

    1 and 2

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 // 1.1. What is the difference between a parameter and an argument?
 
+//a paramter is is the variable that is defined when the function is declared
+//a value is declared within a function
+
 // 1.2. Within a function, what is the difference between return and console.log?
 
+//console.log just prints out the value of a function
+//return returns the values
+
+
 // 1. 3. What are the implications of the ability of a function to return a value?
+
+//if the value can be placed in the function when invokes
+
 
 // 2. calculateCube
 function calculateCube(num) {
     // YOUR CODE HERE
+return num ** 3; }
+{
+
+console.log(calculateCube(5))
+    
 }
 
 // 3. isAVowel
@@ -56,7 +71,7 @@ function sumPositive(arr) {
     // YOUR CODE HERE
 }
 
-module.exports = {
+/*module.exports = {
     calculateCube,
     isAVowel,
     getTwoLengths,
@@ -67,4 +82,4 @@ module.exports = {
     eulerFibo,
     findNeedle,
     sumPositive
-};
+};   */
