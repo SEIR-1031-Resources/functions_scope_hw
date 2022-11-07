04dc89eb9f72f6cfeda9f45f1080514e10dc4c71
solsarr <solomonsarr18@gmail.com>
2022-11-06 20:51:03 -0500

    completed 5

--- app.js
+++ app.js
@@ -51,11 +51,13 @@ function getTwoLengths(word1, word2) {
 // 5. sumArray
 function sumArray(arr) {
     // YOUR CODE HERE
-
-
-    
-    console.log('t')
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];}
+    return sum
+   
 }
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
 
 // 6.1 checkPrime
 function checkPrime(num) {
