fd82835263cfe900d92a06fbffd22c784ffda630
chengu <jeddycheng@gmail.com>
2022-11-08 12:57:30 -0800

    Homework Resubmit 1

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // 1.1. What is the difference between a parameter and an argument?
-
+    //  Parameters are defined along with a function. It's the elements that
+    // the function is requesting to excecute it's code.
+    // Arguments are the elements that we use to call for that specific function.
 // 1.2. Within a function, what is the difference between return and console.log?
-
+    // return stop the function from running after. but console.log is logging into the console
 // 1. 3. What are the implications of the ability of a function to return a value?
-
+    //  We could create or assign those values variables, pass them to other functions as
+    //arguments 
 // 2. calculateCube
 function calculateCube(num) {
     // YOUR CODE HERE
@@ -16,15 +19,15 @@ console.log(calculateCube(5));
 // 3. isAVowel
 function isAVowel(letter) {
     // YOUR CODE HERE
-    if (letter = "a") {
+    if (letter === "a") {
         return true 
-    } else if ( letter = "e") { 
+    } else if ( letter === "e") { 
         return true 
-    } else if ( letter = "i") {
+    } else if ( letter === "i") {
         return true 
-    } else if ( letter = "o") {
+    } else if ( letter === "o") {
         return true 
-    } else if ( letter = "u") {
+    } else if ( letter === "u") {
         return true 
     } else { 
         return false 
@@ -35,8 +38,7 @@ console.log(isAVowel("a"));
 
 
 // 4. getTwoLengths
-function getTwoLengths(word1, word2) {
-    // YOUR CODE HERE
+
     function getTwoLengths(word1, word2) {
         // YOUR CODE HERE
         return [word1.length, word2.length] 
@@ -44,10 +46,10 @@ function getTwoLengths(word1, word2) {
     }
     
     console.log(getTwoLengths("Hank", "Hippopopalous"));
-}
+
 
 // 5. sumArray
-function sumArray(arr) {
+
     // YOUR CODE HERE
     function sumArray(array) {
 
@@ -60,7 +62,7 @@ function sumArray(arr) {
         }
         
         console.log(sumArray([1, 2, 3, 4, 5, 6]));
-}
+
 
 // 6.1 checkPrime
 function checkPrime(num) {
